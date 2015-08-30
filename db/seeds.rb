cd9ebb519da2e3d44be860c8dad1c1e3821b32f8
Roseller M. Velicaria, Jr <r.velicaria.jr@gmail.com>
2015-08-29 21:50:26 -0700

    added more useful testing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(:name => 'PNAwluTuUDuM62pA5oRsRpJ+1hTf+r7XwtLNGKIs3zU51jq2TwYTdS89mwxCc9bwwxb6h4LrKUtYZDrvuf+tOQ==', :password => '6vm9d/yXmFLp7GCIhrv3ZtYagqssHRb2LpznRuJfzqplEeRyXD7V5vlm6Cn/pCWB3Q8M0biPvs4MYGQgGt1t/A==')
+
 user = User.find(1)
-user.favs.create(:subber => 'Commie', :resolution => 720, :title => 'Sailor') unless user.nil?
+user.favs.create(:subber => 'ross_subs_1', :resolution => 720, :title => 'fav test') unless user.nil?
+user.favs.create(:subber => 'ross_subs_2', :resolution => 1080, :title => 'shaolin sumo') unless user.nil?
+user.favs.create(:subber => 'ross_subs_2', :resolution => 1080, :title => 'childrens cartoon') unless user.nil?
+
+Scrape.create(:subber => 'ross_subs_1', :title => 'fav test', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'fav test', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'es ugles', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'barrackie v', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_2', :title => 'fav test', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_2', :title => 'shaolin sumo', :resolution => 1080, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'fav test', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'bebe bebe a bebe', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'lord of the blings', :resolution => 720, :bit_encoding => 8, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+
+Scrape.create(:subber => 'ross_subs_1', :title => 'fav test', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'fav test', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'es ugles', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_1', :title => 'watch me', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_2', :title => 'fav test', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_2', :title => 'this is childrens cartoon. i promise', :resolution => 1080, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'fav test', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'blah', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+Scrape.create(:subber => 'ross_subs_3', :title => 'orc x butcher', :resolution => 720, :bit_encoding => 10, :url => 'example.com', :was_downloaded => false, :publish_date => "01/01/2011")
+
+
