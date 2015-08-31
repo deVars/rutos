151fd934cdac61e4f2b242e9536e587edc5ed8c8
Roseller M. Velicaria, Jr <r.velicaria.jr@gmail.com>
2015-08-31 08:22:16 -0700

    fixed hard_coding of user id when session is missing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 			digested_user = Digest::SHA512.base64digest params[:username]
 			logger.info user
 			if user != nil && user.name == digested_user
-				session[:user_id] = 1
+				session[:user_id] = user.id
 				@json = {success: true, user_id: user.id}
 			end
 		else
