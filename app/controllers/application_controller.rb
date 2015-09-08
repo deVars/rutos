8cd97905705d2ef91e3fc077fc2ca48b6e0b1dd6
Roseller M. Velicaria, Jr <r.velicaria.jr@gmail.com>
2015-09-08 01:30:33 -0700

    [logs] add a logger debug to check request format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 	skip_before_action :verify_authenticity_token, if: :json_request?
 
 	def json_request?
+		logger.debug "request format received: #{request.format}"
+		logger.debug "request format received is json?: #{request.format.json?}"
 		request.format.json?
 	end
 
